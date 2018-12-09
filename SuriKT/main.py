f3694c943d5eeefcdaab8e34d8c3230a1aa53064
Alejandro Guerrero Montoro <alezzzzzz@gmail.com>
2018-12-09 12:16:34 +0100

    Create main.py

---
+++ SuriKT/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 27 12:01:35 2018
+
+@author: Alejandro Guerrero Montoro - alejandro.guerrero.montoro@gmail.com
+
+sudo apt-get install python-paramiko
+apt-get install python-colorama
+
+Este script se ejecuta en local, no funciona si se sube a una sonda.
+"""
